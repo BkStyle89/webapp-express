9cb85e3ba8b443ad26aa5220c3ea5ca9ba84e78e
BkStyle89 <kajzer@hotmail.it>
2026-04-21 12:35:35 +0200

    ccors installed

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const PORT = 3010;
 const postRouter = require('./controller/controller')
 const error500 = require('./middlewares/error500.js')
 
 app.use(express.json());
-
+app.use(cors())
 
 // Base route
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Server del mio blog');
 });
 
