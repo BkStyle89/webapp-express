301f56c77a53c41b4d78d86ec6057fac1bada7ca
BkStyle89 <kajzer@hotmail.it>
2026-04-22 11:07:23 +0200

    cover fixed

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 const PORT = 3010;
 const postRouter = require('./controller/controller')
 const error500 = require('./middlewares/error500.js')
+const path = require('path')
 
 app.use(express.json());
 app.use(cors())
+app.use('/movies_cover', express.static(path.join(__dirname, 'movies_cover')));
 
 // Base route
 app.get('/', (req, res, next) => {
